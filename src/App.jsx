744400acb80a9c75c9553638c5fdcab5aaf5fd90
kumkum27 <127539738+kumkum27@users.noreply.github.com>
2024-11-06 02:01:30 +0530

    Update App.jsx
    removed extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import ImageCards from "./components/ImageCards.jsx";
-import ImageSearch from "./components/ImageSearch.jsx";
+import ImageCards from "./components/ImageCards";
+import ImageSearch from "./components/ImageSearch";
 import github from "./assets/github.png";
 
 function App() {
